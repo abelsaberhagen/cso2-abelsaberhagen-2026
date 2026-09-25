170fa31eac358d22da4347f272a8230a5f7f2eba
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-09-24 23:39:17 -0400

    timing

---
+++ timing/gettimings.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+    switch (argv[0][0]){
+        case '1':
+            printf("case 1");
+        case '2':
+            printf("case 2");
+        case '3':
+            printf("case 3");
+        case '4':
+            printf("case 4");
+        case '5':
+            printf("case 5");
+        case '6':
+            printf("case 6");
+        case '7':
+            printf("case 7");
+        case '8':
+            printf("case 8");
+    }
+
+}
